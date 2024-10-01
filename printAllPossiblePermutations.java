af5fd292d3eca17352f93a47a8bd1eb2a75e7fb9
Bhabesh <bhabeshcse@gmail.com>
2024-10-01 13:09:48 +0530

    Printing all permutations

---
+++ printAllPossiblePermutations.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class printAllPossiblePermutations {
+    // public class Solution {
+    public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        permuter(nums, 0, result);
+        return result;
+    }
+
+    private static void permuter(int[] nums, int start, List<List<Integer>> result) {
+        if (start == nums.length) {
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < nums.length; i++) {
+                list.add(nums[i]);
+            }
+            result.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = start; i < nums.length; i++) {
+            swap(nums, start, i);
+            permuter(nums, start + 1, result);
+            swap(nums, start, i);
+        }
+    }
+    public static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+// }
+    public static void main(String[] args) {
+        // Solution solution = new Solution();
+        int[] nums = {1, 2, 3, 4};
+        List<List<Integer>> result = permute(nums);
+        System.out.println(result.toString());
+    }
+}
