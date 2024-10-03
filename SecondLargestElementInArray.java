6b7c91a8263b649af5cb6319f72a92aa2841ba33
Bhabesh <bhabeshcse@gmail.com>
2024-10-03 11:53:56 +0530

    notes updated

--- SecondLargestElementInArray.java
+++ SecondLargestElementInArray.java
@@ -1,5 +1,6 @@
 public class SecondLargestElementInArray {
     public static void main(String[] args) {
+        //Optimal
         int[] arr = {10, 20, 30, 40, 50};
         int largest = arr[0];
         int secondLargest = arr[0];
@@ -14,6 +15,8 @@ public class SecondLargestElementInArray {
         }
         System.out.println("Second largest element in the array is: " + secondLargest);
     }
+
+    //Brute Force
     // public static int secondLargest(int[] arr) {
     //     int largest = arr[0];
     //     for (int i = 0; i < arr.length; i++) {
